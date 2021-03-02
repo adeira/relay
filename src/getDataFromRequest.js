a8bad2a95cfe308d00ca51062aa6ff80cdf638a4
Martin Zlámal <mrtnzlml@gmail.com>
2021-03-02 09:30:30 -0600

    Merge Relay packages together
    Previously, we tried to split the packages similarly to how Relay does it internally. However, we never finished this effort leaving it fragmented. I'd like to propose merging it back together with these steps to follow:
    
    - cleanup the package (remove things we don't need to wrap because Relay already provides the necessary Flow types)
    - get it ready for next major version Relay 11
    
    adeira-source-id: 10aba4b8307e7ca9ee757885054c752e4bc8d201

---
+++ src/getDataFromRequest.js
@@ -0,0 +1,26 @@
+// @flow
+
+import {
+  createOperationDescriptor,
+  getRequest,
+  type Snapshot,
+  type GraphQLTaggedNode,
+  type Variables,
+} from 'relay-runtime';
+import type { Environment } from 'react-relay';
+
+type Operation = {|
+  +query: GraphQLTaggedNode,
+  +variables: Variables,
+|};
+
+export default function getDataFromRequest(
+  { query, variables }: Operation,
+  environment: Environment,
+): $PropertyType<Snapshot, 'data'> {
+  const request = getRequest(query);
+  const operation = createOperationDescriptor(request, variables);
+
+  const res = environment.lookup(operation.fragment);
+  return res.data;
+}
---
+++ src/loggers/__tests__/RelayEagerLogger.node.test.js
@@ -0,0 +1,34 @@
+/**
+ * @flow
+ * @jest-environment node
+ */
+
+import RelayEagerLogger from '../RelayEagerLogger';
+
+let spy;
+
+beforeEach(() => {
+  spy = jest.spyOn(console, 'log').mockImplementation(jest.fn());
+});
+
+afterEach(() => {
+  spy.mockRestore();
+});
+
+it('does not log in Node.js', () => {
+  RelayEagerLogger({
+    name: 'network.start',
+    transactionID: 1,
+    params: {
+      id: null,
+      name: 'test',
+      operationKind: 'query',
+      text: 'query Lol{id}',
+      metadata: {},
+      cacheID: '',
+    },
+    variables: {},
+  });
+
+  expect(spy).not.toHaveBeenCalled();
+});
